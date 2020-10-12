7e8ab0122847a753015293cd5fda4a30586c1134
Ayoub LAAROBI <ayoub.laarobi@gmail.com>
2020-10-12 20:20:38 +0200

    HomeView: message note about mysql db dalay

--- View/HomeView.Designer.cs
+++ View/HomeView.Designer.cs
@@ -28,15 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(256, 58);
+            this.label1.Location = new System.Drawing.Point(279, 52);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(64, 13);
             this.label1.TabIndex = 0;
@@ -46,7 +48,7 @@
             // button1
             // 
             this.button1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.button1.Location = new System.Drawing.Point(185, 98);
+            this.button1.Location = new System.Drawing.Point(208, 92);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(216, 69);
             this.button1.TabIndex = 1;
@@ -57,17 +59,28 @@
             // 
             // progressBar1
             // 
-            this.progressBar1.Location = new System.Drawing.Point(157, 207);
+            this.progressBar1.Location = new System.Drawing.Point(180, 201);
             this.progressBar1.Name = "progressBar1";
             this.progressBar1.Size = new System.Drawing.Size(266, 23);
             this.progressBar1.TabIndex = 2;
             this.progressBar1.Visible = false;
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Tan;
+            this.label2.Location = new System.Drawing.Point(5, 294);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(634, 39);
+            this.label2.TabIndex = 3;
+            this.label2.Text = resources.GetString("label2.Text");
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(567, 344);
+            this.ClientSize = new System.Drawing.Size(644, 344);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label1);
@@ -83,6 +96,7 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
